cf9a916ecb27184e636773313552246c781647cc
Yassine <yassine.assouale@gmail.com>
2021-10-01 11:19:23 +0200

    Adding delete handler method

--- src/main/java/ya/springboot/sms/controller/StudentController.java
+++ src/main/java/ya/springboot/sms/controller/StudentController.java
@@ -62,4 +62,10 @@ public class StudentController {
 		return "redirect:/students";
 		
 	}
+	
+	@GetMapping("/students/{id}")
+	public String deleteStudent(@PathVariable Long id){
+		studentService.deleteStudentById(id);
+		return "redirect:/students";
+	}
 }
